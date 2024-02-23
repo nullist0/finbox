fdb0a46a7909b8075b04e8ee610b6a0537705c8d
nullist0 <nullist.null@gmail.com>
2024-02-24 02:24:06 +0900

    Make InboxDirectory implement Model

--- app/src/main/java/com/nullist/finbox/domain/InboxDirectory.kt
+++ app/src/main/java/com/nullist/finbox/domain/InboxDirectory.kt
@@ -1,6 +1,12 @@
 package com.nullist.finbox.domain
 
+import java.io.File
+
 data class InboxDirectory(
     val name: String,
-    val files: List<InboxFile>,
-)
+    val path: String,
+): Model() {
+    companion object {
+        fun from(file: File): InboxDirectory = InboxDirectory(file.name, file.path)
+    }
+}
